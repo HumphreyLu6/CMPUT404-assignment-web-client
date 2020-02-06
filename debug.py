c4f45d1b2928fc24d7967c1ddf39cfef3832023b
HumphreyLu6 <zlu@ualberta.ca>
2020-02-05 18:02:59 -0700

    passed freetests.py

--- debug.py
+++ debug.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,21 +26,22 @@ import time
 import urllib.parse
 import json
 
-BASEHOST = '127.0.0.1'
-BASEPORT = 27600 + random.randint(1,100)
+BASEHOST = "127.0.0.1"
+BASEPORT = 27600 + random.randint(1, 100)
 
 
 httpclass = httpclient
-#import mysolution
-#httpclass = mysolution
+# import mysolution
+# httpclass = mysolution
 
 # Sorry but in Python this comes out of the box!
 class MyHTTPHandler(http.server.BaseHTTPRequestHandler):
-    post = None 
+    post = None
     get = None
+
     def do_POST(self):
         try:
-            if (self.post == None):
+            if self.post == None:
                 return None
             else:
                 return self.post()
@@ -51,7 +52,7 @@ class MyHTTPHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             print("GET %s\n" % self.path)
-            if (self.get == None):
+            if self.get == None:
                 return None
             else:
                 return self.get()
@@ -59,14 +60,17 @@ class MyHTTPHandler(http.server.BaseHTTPRequestHandler):
             print("Exception %s\n" % e)
             raise e
 
-def make_http_server(host = BASEHOST, port = BASEPORT):
-    return http.server.HTTPServer( (host, port) , MyHTTPHandler)
 
-# # always returns 404
-# def nothing_available(self):
-#     self.send_error(404, "File not found")
-#     self.end_headers()
-#     self.wfile.write(bytes("","utf-8"))
+def make_http_server(host=BASEHOST, port=BASEPORT):
+    return http.server.HTTPServer((host, port), MyHTTPHandler)
+
+
+# always returns 404
+def nothing_available(self):
+    self.send_error(404, "File not found")
+    self.end_headers()
+    self.wfile.write(bytes("", "utf-8"))
+
 
 # repeats your path back
 def echo_path_get(self):
@@ -74,54 +78,56 @@ def echo_path_get(self):
     self.send_header("Content-type", "text/plain")
     self.end_headers()
     print(type(self.wfile))
-    self.wfile.write(bytes("%s\n" % self.path,"utf-8"))
-
-# # repeats your post back as json
-# def echo_post(self):
-#     length = int(self.headers['Content-Length'])
-#     post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-#     self.send_response(200)
-#     self.send_header("Content-type", "application/json")
-#     self.end_headers()
-#     self.wfile.write(bytes(json.dumps(post_data),"utf-8"))
-
-# def header_check(self):
-#     response = 200
-#     errors = []
-#     if 'Host' not in self.headers:
-#         response = 400
-#         errors.append("No Host header found")
-#     self.send_response(response)
-#     self.send_header("Content-type", "application/json")
-#     self.end_headers()
-#     self.wfile.write(bytes(json.dumps(errors),"utf-8"))
-
-# def die_on_method(self):
-#     response = 405
-#     errors = []
-#     errors.append("Method Not Allowed")
-#     if 'Host' not in self.headers:
-#         errors.append("No Host header found")
-#     self.send_response(response)
-#     self.send_header("Content-type", "application/json")
-#     self.end_headers()
-#     self.wfile.write(bytes(json.dumps(errors),"utf-8"))
-
-# def post_header_check(self):
-#     response = 200
-#     errors = []
-#     if 'Host' not in self.headers:
-#         response = 400
-#         errors.append("No Host header found")
-#     if 'Content-length' not in self.headers:
-#         response = 400
-#         errors.append("No Content-Length header found")
-#     self.send_response(response)
-#     self.send_header("Content-type", "application/json")
-#     self.end_headers()
-#     self.wfile.write(bytes(json.dumps(errors),"utf-8"))
+    self.wfile.write(bytes("%s\n" % self.path, "utf-8"))
 
 
+# repeats your post back as json
+def echo_post(self):
+    length = int(self.headers["Content-Length"])
+    post_data = urllib.parse.parse_qs(self.rfile.read(length).decode("utf-8"))
+    self.send_response(200)
+    self.send_header("Content-type", "application/json")
+    self.end_headers()
+    self.wfile.write(bytes(json.dumps(post_data), "utf-8"))
+
+
+def header_check(self):
+    response = 200
+    errors = []
+    if "Host" not in self.headers:
+        response = 400
+        errors.append("No Host header found")
+    self.send_response(response)
+    self.send_header("Content-type", "application/json")
+    self.end_headers()
+    self.wfile.write(bytes(json.dumps(errors), "utf-8"))
+
+
+def die_on_method(self):
+    response = 405
+    errors = []
+    errors.append("Method Not Allowed")
+    if "Host" not in self.headers:
+        errors.append("No Host header found")
+    self.send_response(response)
+    self.send_header("Content-type", "application/json")
+    self.end_headers()
+    self.wfile.write(bytes(json.dumps(errors), "utf-8"))
+
+
+def post_header_check(self):
+    response = 200
+    errors = []
+    if "Host" not in self.headers:
+        response = 400
+        errors.append("No Host header found")
+    if "Content-length" not in self.headers:
+        response = 400
+        errors.append("No Content-Length header found")
+    self.send_response(response)
+    self.send_header("Content-type", "application/json")
+    self.end_headers()
+    self.wfile.write(bytes(json.dumps(errors), "utf-8"))
 
 
 class TestHTTPClient(unittest.TestCase):
@@ -130,20 +136,20 @@ class TestHTTPClient(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        '''Cache the httpd server and run it as a thread'''
-        if (TestHTTPClient.httpd == None):
+        """Cache the httpd server and run it as a thread"""
+        if TestHTTPClient.httpd == None:
             try:
                 self.thread = threading.Thread(target=self.run_server).start()
                 time.sleep(1)
             except Exception as e:
                 print(e)
                 print("setUP: Thread died")
-                raise(e)
+                raise (e)
 
     @classmethod
     def run_server(self):
-        '''run the httpd server in a thread'''
-        try:    
+        """run the httpd server in a thread"""
+        try:
             socketserver.TCPServer.allow_reuse_address = True
             http.server.HTTPServer.allow_reuse_address = True
             TestHTTPClient.httpd = make_http_server()
@@ -153,8 +159,6 @@ class TestHTTPClient(unittest.TestCase):
         except Exception as e:
             print(e)
             print("run_server: Thread died")
-
-
 
     # def test404GET(self):
     #     '''Test against 404 errors'''
@@ -172,25 +176,25 @@ class TestHTTPClient(unittest.TestCase):
     #     self.assertTrue(req != None, "None Returned!")
     #     self.assertTrue(req.code == 404)
 
-    def testGET(self):
-        '''Test HTTP GET'''
-        MyHTTPHandler.get = echo_path_get
-        http = httpclass.HTTPClient()
-        path = "abcdef/gjkd/dsadas"
-        url = "http://%s:%d/%s" % (BASEHOST,BASEPORT, path)
-        req = http.GET( url )
-        self.assertTrue(req != None, "None Returned!")
-        self.assertTrue(req.code == 200)
-        self.assertTrue(req.body.find(path)>=0, "Data: [%s] " % req.body)
-
-    # def testGETHeaders(self):
-    #     '''Test HTTP GET Headers'''
-    #     MyHTTPHandler.get = header_check
-    #     MyHTTPHandler.post = die_on_method
+    # def testGET(self):
+    #     '''Test HTTP GET'''
+    #     MyHTTPHandler.get = echo_path_get
     #     http = httpclass.HTTPClient()
     #     path = "abcdef/gjkd/dsadas"
     #     url = "http://%s:%d/%s" % (BASEHOST,BASEPORT, path)
     #     req = http.GET( url )
+    #     self.assertTrue(req != None, "None Returned!")
+    #     self.assertTrue(req.code == 200)
+    #     self.assertTrue(req.body.find(path)>=0, "Data: [%s] " % req.body)
+
+    # def testGETHeaders(self):
+    #     """Test HTTP GET Headers"""
+    #     MyHTTPHandler.get = header_check
+    #     MyHTTPHandler.post = die_on_method
+    #     http = httpclass.HTTPClient()
+    #     path = "abcdef/gjkd/dsadas"
+    #     url = "http://%s:%d/%s" % (BASEHOST, BASEPORT, path)
+    #     req = http.GET(url)
     #     self.assertTrue(req != None, "None Returned!")
     #     self.assertTrue(req.code == 200)
 
@@ -202,39 +206,38 @@ class TestHTTPClient(unittest.TestCase):
     #     path = "abcdef/gjkd/dsadas"
     #     url = "http://%s:%d/%s" % (BASEHOST,BASEPORT, path)
     #     req = http.POST( url )
+    #     print("url = {0}\nreq = {1}".format(url, req))
     #     self.assertTrue(req != None, "None Returned!")
     #     self.assertTrue(req.code == 200,"Code is %s but I wanted a 200 OK" % req.code)
 
-        
-        
     # consider disabling this test until everything else works
-    # def testInternetGets(self):
-    #     '''Test HTTP Get in the wild, these webservers are far less
-    #        forgiving'''
-    #     MyHTTPHandler.get = echo_path_get
-    #     http = httpclass.HTTPClient()        
-    #     urls = [
-    #         "http://www.cs.ualberta.ca/",
-    #         "http://softwareprocess.es/static/SoftwareProcess.es.html",
-    #         "http://c2.com/cgi/wiki?CommonLispHyperSpec",
-    #         "http://slashdot.org"
-    #         ]
-    #     for url in urls:
-    #         try:
-    #             req = http.GET( url )
-    #         except Exception as e:
-    #             print("An Exception was thrown for %s" % url)
-    #             self.assertTrue( False, "An Exception was thrown for %s %s" % (url,e))
-    #         self.assertTrue(req != None, "None Returned! %s" % url)
-    #         self.assertTrue(req.code == 200 or 
-    #                         req.code == 301 or
-    #                         req.code == 302,
-    #                         "Code: %s for %s" % (req.code, url))
-    #         if (req.code == 200):
-    #             self.assertTrue(req.body.find("DOCTYPE")>=0 or 
-    #                             req.body.find("<body")>=0 , 
-    #                             "%s Data: [%s] " % (url,req.body))
-    
+    def testInternetGets(self):
+        '''Test HTTP Get in the wild, these webservers are far less
+           forgiving'''
+        MyHTTPHandler.get = echo_path_get
+        http = httpclass.HTTPClient()
+        urls = [
+            "http://www.cs.ualberta.ca/",
+            "http://softwareprocess.es/static/SoftwareProcess.es.html",
+            "http://c2.com/cgi/wiki?CommonLispHyperSpec",
+            "http://slashdot.org"
+            ]
+        for url in urls:
+            try:
+                req = http.GET( url )
+            except Exception as e:
+                print("An Exception was thrown for %s" % url)
+                self.assertTrue( False, "An Exception was thrown for %s %s" % (url,e))
+            self.assertTrue(req != None, "None Returned! %s" % url)
+            self.assertTrue(req.code == 200 or
+                            req.code == 301 or
+                            req.code == 302,
+                            "Code: %s for %s" % (req.code, url))
+            if (req.code == 200):
+                self.assertTrue(req.body.find("DOCTYPE")>=0 or
+                                req.body.find("<body")>=0 ,
+                                "%s Data: [%s] " % (url,req.body))
+
     # def testPOST(self):
     #     '''Test HTTP POST with an echo server'''
     #     MyHTTPHandler.post = echo_post
@@ -258,15 +261,16 @@ class TestHTTPClient(unittest.TestCase):
     #         self.assertTrue(args[key] == outargs[key][0], "Key [%s] not found" % key)
 
     @classmethod
-    def tearDownClass(self):        
-        if (TestHTTPClient.httpd!=None):
+    def tearDownClass(self):
+        if TestHTTPClient.httpd != None:
             print("HTTP Shutdown in tearDown\n")
             TestHTTPClient.httpd.shutdown()
             TestHTTPClient.httpd.server_close()
             time.sleep(1)
 
+
 def test_test_webserver():
-    print("http://%s:%d/dsadsadsadsa\n" % (BASEHOST,BASEPORT) )
+    print("http://%s:%d/dsadsadsadsa\n" % (BASEHOST, BASEPORT))
     MyHTTPHandler.get = echo_path_get
     MyHTTPHandler.post = echo_post
     httpd = make_http_server()
@@ -275,5 +279,6 @@ def test_test_webserver():
     finally:
         httpd.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,31 +21,42 @@
 import sys
 import socket
 import re
+
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+# https://stackoverflow.com/questions/21628852/changing-hostname-in-a-url
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
-class HTTPClient(object):
-    def get_host_port(self,url):
-        parse_result = urllib.parse.urlparse(url)
-        #print(url, '\n', parse_result)
-        print("debug", parse_result.hostname, parse_result.path, parse_result.port)
-        if parse_result.hostname == None:
-            raise Exception('No hostname found')
-        else:
-            self.host = parse_result.hostname
 
-        if parse_result.port == None:
-            self.port = 80
-        else:
-            self.port = int(parse_result.port)
+UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
+
+class HTTPClient(object):
+    def parse_url(self, url):
+        parse_result = urllib.parse.urlparse(url)
+        if parse_result.hostname == None:
+            raise Exception("No hostname found")
+
+        port = 80
+        if parse_result.port != None:
+            port = int(parse_result.port)
+
+        path = parse_result.path
+        if path == "":
+            path = "/"
+
+        self.url_pr = parse_result._replace(
+            netloc="{}:{}".format(parse_result.hostname, port), path=path
+        )
+
         return None
 
     def connect(self, host, port):
@@ -54,17 +65,21 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        lines = data.splitlines()
+        code = int(lines[0].split(" ")[1])
+        return code
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        headers = data.split("\r\n\r\n")[0]
+        return headers
 
     def get_body(self, data):
-        return None
-    
+        body = data.split("\r\n\r\n")[1]
+        return body
+
     def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
-        
+        self.socket.sendall(data.encode("utf-8"))
+
     def close(self):
         self.socket.close()
 
@@ -74,37 +89,72 @@ class HTTPClient(object):
         done = False
         while not done:
             part = sock.recv(1024)
-            if (part):
+            if part:
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode("utf-8")
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        self.parse_url(url)
+        request = "GET {0} HTTP/1.1\r\nUser-Agent:{2}\r\nHost:{1}\r\nConnection:close\r\n\r\n".format(
+            self.url_pr.path, self.url_pr.hostname, UserAgent
+        )
+
+        self.connect(self.url_pr.hostname, self.url_pr.port)
+
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+
+        self.headers = self.get_headers(data)
+        code = self.get_code(self.headers)
+        body = self.get_body(data)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        self.parse_url(url)
+        if args == None:
+            args = ""
+        encoded_string = urllib.parse.urlencode(args)
+        request = (
+            "POST {0} HTTP/1.1\r\n"
+            "Host: {1}\r\n"
+            "Content-Type: application/x-www-form-urlencoded\r\n"
+            "Content-Length: {2}\r\n"
+            "Connection: close\r\n\r\n{3}".format(
+                self.url_pr.path,
+                self.url_pr.hostname,
+                len(encoded_string),
+                encoded_string,
+            )
+        )
+
+        self.connect(self.url_pr.hostname, self.url_pr.port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+
+        self.headers = self.get_headers(data)
+        code = self.get_code(self.headers)
+        body = self.get_body(data)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
-        if (command == "POST"):
-            return self.POST( url, args )
+        if command == "POST":
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
-    if (len(sys.argv) <= 1):
+    if len(sys.argv) <= 1:
         help()
         sys.exit(1)
-    elif (len(sys.argv) == 3):
-        print("Client: ", sys.argv)
-        client.get_host_port(sys.argv[2])
-        print(client.command( sys.argv[2], sys.argv[1] ))
+    elif len(sys.argv) == 3:
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
